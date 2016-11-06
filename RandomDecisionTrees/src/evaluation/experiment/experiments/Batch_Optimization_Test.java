0d7dc8e22ce27f5245977bc4fe066051a95cbbef
mkulessa <mkulessa@4e53c0a0-b128-49ab-a70e-737cf4b15d57>
2016-11-06 15:52:10 +0000

    Unzip multilabel-data files and add optimization-experiment.

--- RandomDecisionTrees/src/evaluation/experiment/experiments/Batch_Optimization_Test.java
+++ RandomDecisionTrees/src/evaluation/experiment/experiments/Batch_Optimization_Test.java
@@ -1,59 +1,32 @@
 package evaluation.experiment.experiments;
 
-import evaluation.EvaluationType;
 import evaluation.dataset.DatasetType;
-import evaluation.experiment.CompareExperiment;
 import evaluation.experiment.ExperimentType;
-
+import evaluation.experiment.Optimization;
 import rdt.model.ModelType;
-import rdt.model.MultilabelChainEnsemble.ChainType;
-import rdt.model.MultilabelChainEnsemble.PredictType;
 
-public class Batch_Optimization_Test extends CompareExperiment {
-	
+public class Batch_Optimization_Test extends Optimization{
+
 	@Override
-	protected ModelType[] getModelTypes() {
-		return new ModelType[]{
-				ModelType.SPARSE_MULTILABEL_CHAIN_ENSEMBLE,
-		};
+	protected ModelType getModelType() {
+		return ModelType.BATCH_ENSEMBLE;
 	}
 
 	@Override
-	protected Object[][][] getParameters() {
-		return new Object[][][]{
-			//Model 1
-			{
-				new Integer[]{50},		//Trees
-				new Integer[]{20},				//maxDeep
-				new Integer[]{5},				//maxS
-				new Long[]{(long) 1},			//randomSeed	
-				new Double[]{0.2},					//percentageLabels
-				new Double[]{1.0}, 				//percentageActiveLabels
-				new Integer[]{10000},				//numChainPredict
-				new PredictType[]{PredictType.PERCENTAGE_CHAIN},				
-				new ChainType[]{ChainType.HYBRID},	
-				new Integer[]{10},
-				
-			},
-		
+	protected Object[][] getParameters() {
+		return new Object[][]{
+				new Integer[]{10,15},		//Trees
+				new Integer[]{15,20},		//maxDeep
+				new Integer[]{5},		//maxS
+				new Long[]{(long) 1},	//randomSeed 
 		};
 	}
 
 	@Override
 	public DatasetType[] getDatasets() {
 		return new DatasetType[]{
-				DatasetType.MULTILABEL_ENRON
+				DatasetType.MULTILABEL_SCENE,
 		};
-	}
-
-	@Override
-	protected int[] getFocuses() {
-		return new int[]{1};
-	}
-
-	@Override
-	protected EvaluationType getEvaluationType() {
-		return EvaluationType.CROSS_VALIDATION;
 	}
 
 	@Override
